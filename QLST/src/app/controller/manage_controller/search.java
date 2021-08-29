8bbed6187fb8255ba8e23eb7170c1c145006e5c7
tminhhuy19 <tminhhuy19@gmail.com>
2021-08-30 02:13:49 +0700

    commit

---
+++ QLST/src/app/controller/manage_controller/search.java
@@ -0,0 +1,107 @@
+package app.controller.manage_controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+public class search {
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private DatePicker from;
+
+    @FXML
+    private DatePicker to;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    void Quarter_1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Quarter_2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Quarter_3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Quarter_4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T10(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T11(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T12(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void T9(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_date(ActionEvent event) {
+
+    }
+
+}
