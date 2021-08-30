368f948653893cb4d2bb2c7acc460c6b2f077f12
nguyentanduc277 <88288513+nguyentanduc277@users.noreply.github.com>
2021-08-30 09:58:01 +0700

    S
    S

--- QLST/src/app/controller/employee_controller/Change_shift_employee.java
+++ QLST/src/app/controller/employee_controller/Change_shift_employee.java
@@ -14,10 +14,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.DecimalFormat;
 import java.util.Iterator;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -35,6 +43,7 @@ import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 
+import app.controller.employee_controller.Change_shift_email;
 import app.controller.homepage.Info_Employee;
 import app.dao.connectDB;
 import app.model.ChangeShift;
@@ -59,6 +68,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Change_shift_employee implements Initializable{
@@ -87,6 +97,18 @@ public class Change_shift_employee implements Initializable{
 	    @FXML
 	    private TableColumn<ChangeShift, Integer> col_no_shift_id;
 	    
+		@FXML
+	    private AnchorPane View_email;
+
+	    @FXML
+	    private TextField email_name;
+
+	    @FXML
+	    private TextField email_email;
+
+	    @FXML
+	    private TextArea email_content;
+
 	    @FXML
 	    private Label ltotal;
 	    
@@ -117,29 +139,124 @@ public class Change_shift_employee implements Initializable{
 	        table_changeshift.setItems(listM);
 	    }
 	    
-	@FXML
-void exit(MouseEvent event) {
-	try {
-		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("../../ui/homepage/Home_Employee.fxml"));
-		Parent parent;
+//	    @FXML
+//		void Sent(ActionEvent event) throws Exception {
+////			conn = connectDB.ConnectDb();
+//			try {
+//				if (email.getText().trim().equals("")) {
+//					JOptionPane.showMessageDialog(null, "Email cannot be blank!!");
+//					System.out.println("trong r");
+//				} else {
+//					String input_text = email.getText();
+//					String encryptedString = null;
+//					encryptedString = encryptorAES.encrypt(input_text);
+//
+//					pst = conn.prepareStatement(sql);
+//					pst.setString(1, name.getText());
+//					pst.setString(2, email.getText());
+//					pst.setString(3, title.getText());
+//					pst.setString(4, email.getText());
+//					pst.setString(5, date_switch.getEditor().getText());
+//					pst.setString(6, shift_change_date.getEditor().getText());
+//					pst.setString(7, content.getText());
+//					pst.setString(8, encryptedString);
+//					pst.execute();
+////					--------------------------------------------------
+//					// Your gmail address
+//					String myAccountEmail = "crmgroupapp@gmail.com";
+//					// Your gmail password
+//					String password = "crmapp0123123";
+//
+//					Properties properties = new Properties();
+//
+//					properties.put("mail.smtp.auth", "true");
+//					properties.put("mail.smtp.starttls.enable", "true");
+//					properties.put("mail.smtp.host", "smtp.gmail.com");
+//					properties.put("mail.smtp.port", "587");
+//
+//					Session session = Session.getInstance(properties, new Authenticator() {
+//						@Override
+//						protected PasswordAuthentication getPasswordAuthentication() {
+//							return new PasswordAuthentication(myAccountEmail, password);
+//						}
+//					});
+//	    try {
+//			System.out.println("Sending.");					
+//			Message message = new MimeMessage(session);
+//			message.setFrom(new InternetAddress(myAccountEmail));
+//			message.setRecipient(Message.RecipientType.TO, new InternetAddress(email.getText()));
+//			message.setSubject("Change Shift:");
+//			String htmlCode = "<h2>"
+//					+ "			Super market group send to <i> <u>" + name.getText()+ "</u> </i> . <br> "
+//					+ "        Change shift."
+//					+ "    </h2>"
+//					+ "    <h3>Title: <i> <u>" + title.getValue()+ "</u> </i> <br/> "
+//					+ "        Main shift: <i> <u>" + main_shift.getText()+ "</u> </i> <br>"
+//					+ "        Switch shift:<i> <u>" + switch_shift.getText()+ "</u> </i> <br/> "
+//					+ "        Date switch:<i> <u>" + date_switch.getEditor().getText() + "</u> </i> <br/>"
+//					+ "        Shift change date:<i> <u>" + shift_change_date.getEditor().getText() + "</u> </i> <br/>"
+//					+ "    </h3>"
+//					+ "    <p>"
+//					+ "        If you have any questions, please contact: 0252112002 . <br>"
+//					+ "        wish you good day."
+//					+ "    </p>";
+//			message.setContent(htmlCode, "text/html; charset=utf-8");
+//			Transport.send(message);
+//			JOptionPane.showMessageDialog(null, "Send Successfully");
+//			System.out.println("Message sent successfully");
+//		} catch (Exception ex) {
+//			// TODO: handle exception
+//			ex.printStackTrace();
+//
+//	    }
+	    
+	    private static String E_name,E_email;
+	    @FXML
+	    void Changeshift_email(MouseEvent event) {
+		    try {
+		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../ui/employee/change_shift_email.fxml"));
+						E_name=email_name.getText();
+						E_email=email_email.getText();
+		                Parent root = (Parent) fxmlLoader.load();
+		                System.out.println(E_name+E_email);
+		                Stage stage = new Stage();
+		            	Change_shift_email email=fxmlLoader.getController();
+		            	email.getInformation(E_name, E_email);
+		                stage.setScene(new Scene(root));  
+		                stage.show();             
+		                UpdateTable_changeshift();
+		                
+		        } catch(Exception e) {
+		        	
+		           e.printStackTrace();
+		          }
+	    }
 
-		parent = loader.load();
-
-		Scene scene = new Scene(parent);
-		stage.setScene(scene);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		UpdateTable_changeshift();
-	}
+	    
+	    
+		@FXML
+		void exit(MouseEvent event) {
+			try {
+				Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(getClass().getResource("../../ui/homepage/Home_Employee.fxml"));
+				Parent parent;
+		
+				parent = loader.load();
+		
+				Scene scene = new Scene(parent);
+				stage.setScene(scene);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	
-	
-}
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			// TODO Auto-generated method stub
+			UpdateTable_changeshift();
+		}
+		
+		
+	}
