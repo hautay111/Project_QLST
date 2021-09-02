5d6bd54ed916c1b22d69af7c9429ff81c5407fe1
tminhhuy19 <tminhhuy19@gmail.com>
2021-09-02 23:25:47 +0700

    commit 25 file

---
+++ QLST/src/app/controller/manage_controller/search_dashhoard/a.java
@@ -0,0 +1,12 @@
+package app.controller.manage_controller.search_dashhoard;
+
+public class a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(java.time.LocalDate.now());
+		System.out.println(java.time.LocalDateTime.now()); 
+		
+	}
+
+}
