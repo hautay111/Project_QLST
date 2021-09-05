dde5d01f3542fe7ff1b5356ffe8197eac58b297f
tminhhuy19 <tminhhuy19@gmail.com>
2021-09-06 03:21:41 +0700

    commit 3h21 ngay 6/9

--- QLST/src/app/controller/employee_controller/security.java
+++ QLST/src/app/controller/employee_controller/security.java
@@ -1,4 +1,4 @@
-package app.controller.homepage;
+package app.controller.employee_controller;
 
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -14,6 +14,11 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.swing.JOptionPane;
+
+import app.controller.employee_controller.Bill_Controller.Bill_Controller;
+import app.controller.homepage.EncryptorAES;
+import app.controller.homepage.Home_Employee;
+import app.controller.homepage.Home_Manage;
 import app.dao.connectDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -22,18 +27,24 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Login {
-	
+public class security {
 	EncryptorAES encryptorAES = new EncryptorAES();
 	Connection conn =null;
     ResultSet rs = null;
     PreparedStatement pst = null;
-	
+    
+    @FXML
+    private AnchorPane main;
+    
+    @FXML
+    private Label order_id;
+
     @FXML
     private TextField txt_username;
 
@@ -45,31 +56,11 @@ public class Login {
 
     @FXML
     private CheckBox btn_showpass;
-    
-    @FXML
-    void show_pass(ActionEvent event) {
-    	if(btn_showpass.isSelected()) {
-    		txt_passhiden.setText(txt_pass.getText());
-    		txt_passhiden.setVisible(true);
-    		txt_pass.setVisible(false);
-    		return;
-    	}
-    	txt_pass.setText(txt_passhiden.getText());
-    	txt_pass.setVisible(true);
-    	txt_passhiden.setVisible(false);
-    }
-    private String getPassword(){
-        if(txt_passhiden.isVisible()){
-            return txt_passhiden.getText();
-        } else {
-            return txt_pass.getText();
-        }
-    }
     private static String id,title_name,user;
     private static Integer title_id,status;
+    
     @FXML
-    void btn_login(ActionEvent event) throws SQLException, IOException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
-    	
+    void btn_login(ActionEvent event) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException, SQLException, IOException {
     	String pass=getPassword();
 		String enBase64=encode(pass);
         String key = "65 12 12 12 12 12 12 12 12 12 12 12 12 12 12 11";
@@ -98,48 +89,54 @@ public class Login {
 					if (rs1.next()) {
 						title_name=rs1.getString("title_name");
 						System.out.println(title_name);
-						Stage stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
-						FXMLLoader loader=new FXMLLoader();
-						loader.setLocation(getClass().getResource("../../ui/homepage/Home_Employee.fxml"));							
-						Parent parent=loader.load();
-						String sql = "update employee set emp_status=1 where emp_id='"+id+"' ";
-			            pst= conn.prepareStatement(sql);
-			            pst.execute();
-			            Home_Employee home=loader.getController();
-			            home.getId(id,user,title_name);
-						Scene scene=new Scene(parent);				
-						stage.setScene(scene);
+//						Stage stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+//						FXMLLoader loader=new FXMLLoader();
+//						loader.setLocation(getClass().getResource("../../ui/homepage/Home_Employee.fxml"));							
+//						Parent parent=loader.load();
+//						String sql = "update employee set emp_status=1 where emp_id='"+id+"' ";
+//			            pst= conn.prepareStatement(sql);
+//			            pst.execute();
+//			            Home_Employee home=loader.getController();
+//			            home.getId(id,user,title_name,image);
+//						Scene scene=new Scene(parent);				
+//						stage.setScene(scene);
+						conn = connectDB.ConnectDb();
+					      String sql1q = "delete from orders where order_id = ?";					         
+					              pst = conn.prepareStatement(sql1q);
+					              pst.setString(1, a);
+					              pst.execute();
+					              JOptionPane.showMessageDialog(null, "Delete Successfully.");
+					              Stage stage = (Stage)main.getScene().getWindow();
+					              // do what you have to do
+					              stage.close();
 					}
-					String sql2="Select * from title where title_id= '"+title_id+"' and title_name not like '%emp%'";				
-					ResultSet rs2 = pst.executeQuery(sql2);
-					if (rs2.next()) {
-						title_name=rs2.getString("title_name");
-						System.out.println(title_name);
-						Stage stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
-						FXMLLoader loader=new FXMLLoader();
-						loader.setLocation(getClass().getResource("../../ui/homepage/Home_Manage.fxml"));								
-						Parent parent=loader.load();
-						String sql = "update employee set emp_status=1 where emp_id='"+id+"' ";
-			            pst= conn.prepareStatement(sql);
-			            pst.execute();
-			            Home_Manage home=loader.getController();
-			            home.getId(id,user,title_name);
-						Scene scene=new Scene(parent);				
-						stage.setScene(scene);
-					}
+					
 				}							
 			}else {
         		JOptionPane.showMessageDialog(null, "Check username or password.");	
         	}
 			conn.close();
-	}
-    
-    @FXML
-    void logout(MouseEvent event) {
-    	System.exit(0);
     }
-    public void loadHome(String ui) {
-    	
+
+    @FXML
+    void show_pass(ActionEvent event) {
+    	if(btn_showpass.isSelected()) {
+    		txt_passhiden.setText(txt_pass.getText());
+    		txt_passhiden.setVisible(true);
+    		txt_pass.setVisible(false);
+    		return;
+    	}
+    	txt_pass.setText(txt_passhiden.getText());
+    	txt_pass.setVisible(true);
+    	txt_passhiden.setVisible(false);
+    }
+    
+    private String getPassword(){
+        if(txt_passhiden.isVisible()){
+            return txt_passhiden.getText();
+        } else {
+            return txt_pass.getText();
+        }
     }
 	/*--------------------------------------------------------------------------*/
 	public static String encode(String str) {
@@ -154,5 +151,11 @@ public class Login {
 		byte[] decoded = decode.decode(str);
 		return new String(decoded);
 
+	}
+	private static String a;
+	public void getOrderId(String ID_ORDER) {
+		order_id.setText(ID_ORDER);
+		a=order_id.getText();
+		System.out.println("ID ORDER: "+a);
 	}
 }
